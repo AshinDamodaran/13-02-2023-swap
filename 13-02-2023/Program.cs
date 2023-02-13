118fde1df9b30429d6e62101c1fc491e2a8045a4
Ashin Damodaran <ashin.damodaran@experionglobal.local>
2023-02-13 15:02:50 +0530

    13-02-2023swap

---
+++ 13-02-2023/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");  
+  public class Example
+{
+    public static void Main(string[] args)
+    {
+        int x = 20, y = 30;
+        Console.WriteLine("Before swap:");
+        Console.WriteLine("x = " + x + " and y = " + y);
+        x = x * y;
+        y = x / y;
+        x = x / y;
+        Console.WriteLine("After swap:");
+        Console.WriteLine("x = " + x + " and y = " + y);
+    }
+}
